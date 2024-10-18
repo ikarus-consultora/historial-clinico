fff1cf38293aa7bbcbfd85a07a34fefb5006c809
damian <dellacortedamian@gmail.com>
2024-10-18 16:37:31 -0300

    vite config 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/historial-clinico/',
+  base: 'https://ikarus-consultora.github.io/historial-clinico/',
   plugins: [react()],
   resolve: {
     alias: {
